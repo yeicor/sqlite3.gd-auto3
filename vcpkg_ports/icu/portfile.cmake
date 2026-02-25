31215d9a485308cee74004c940678638e3968d0c
Yeicor <yeicor+no-reply@github.com>
2026-02-25 22:08:25 +0100

    Enable fail-fast and adjust vcpkg port patches
    In vcpkg ports disable an upstream Mingw-specific conditional in icu by
    forcing it to FALSE, and update sqlite3 patching to point to patched
    files and append Emscripten compile options for PIC and atomics.

--- vcpkg_ports/icu/portfile.cmake
+++ vcpkg_ports/icu/portfile.cmake
@@ -1,10 +1,13 @@
 # Copy upstream port files except portfile.cmake
 file(COPY "${VCPKG_ROOT_DIR}/ports/icu/" DESTINATION "${CMAKE_CURRENT_LIST_DIR}" PATTERN "portfile.cmake" EXCLUDE)
 
-# Patch upstream portfile.cmake to add emscripten args for threads mode and PIC always on
+# Patch upstream portfile.cmake
 file(READ "${VCPKG_ROOT_DIR}/ports/icu/portfile.cmake" upstream_content)
 if(VCPKG_TARGET_IS_EMSCRIPTEN)
     string(REPLACE "vcpkg_list(SET CONFIGURE_OPTIONS)" "vcpkg_list(SET CONFIGURE_OPTIONS)\nvcpkg_list(APPEND CONFIGURE_OPTIONS \"--with-pic\" \"CFLAGS=-fPIC -matomics -mbulk-memory\" \"CXXFLAGS=-fPIC -matomics -mbulk-memory\" \"LDFLAGS=-fPIC -matomics -mbulk-memory\")" upstream_content "${upstream_content}")
+endif()
+if(VCPKG_TARGET_IS_MINGW)
+    string(REPLACE "if(CMAKE_HOST_WIN32 AND VCPKG_TARGET_IS_MINGW AND NOT HOST_TRIPLET MATCHES \"mingw\")" "if(FALSE AND CMAKE_HOST_WIN32 AND VCPKG_TARGET_IS_MINGW AND NOT HOST_TRIPLET MATCHES \"mingw\")" upstream_content "${upstream_content}")
 endif()
 file(WRITE "${CMAKE_CURRENT_LIST_DIR}/portfile_upstream_patched.cmake" "${upstream_content}")
 
--- vcpkg_ports/sqlite3/portfile.cmake
+++ vcpkg_ports/sqlite3/portfile.cmake
@@ -1,12 +1,12 @@
 # Copy upstream port files except portfile.cmake
 file(COPY "${VCPKG_ROOT_DIR}/ports/sqlite3/" DESTINATION "${CMAKE_CURRENT_LIST_DIR}" PATTERN "portfile.cmake" EXCLUDE)
 
-# Patch upstream portfile.cmake to use the files from this directory instead of the original ones
+# Patch upstream portfile.cmake to point to the patched files
 file(READ "${VCPKG_ROOT_DIR}/ports/sqlite3/portfile.cmake" upstream_content)
 string(REPLACE "\${CMAKE_CURRENT_LIST_DIR}" "${CMAKE_CURRENT_LIST_DIR}" upstream_content "${upstream_content}")
 file(WRITE "${CMAKE_CURRENT_LIST_DIR}/portfile_upstream_patched.cmake" "${upstream_content}")
 
-# Patch upstream portfile.cmake to add emscripten args for threads mode and PIC always on
+# Patch upstream CMakeLists.txt
 file(READ "${VCPKG_ROOT_DIR}/ports/sqlite3/CMakeLists.txt" upstream_content)
 if(VCPKG_TARGET_IS_EMSCRIPTEN)
     string(APPEND upstream_content "\ntarget_compile_options(sqlite3 PRIVATE -fPIC -matomics -mbulk-memory)")
