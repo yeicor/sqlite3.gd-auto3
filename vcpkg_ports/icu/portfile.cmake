7eaebbd257dc53b9bc78215e11071ff13c2b1465
Yeicor <yeicor+no-reply@github.com>
2026-02-25 20:13:00 +0100

    Add Emscripten thread flags and enable PIC

--- vcpkg_ports/icu/portfile.cmake
+++ vcpkg_ports/icu/portfile.cmake
@@ -1,7 +1,7 @@
 # Copy upstream port files except portfile.cmake
 file(COPY "${VCPKG_ROOT_DIR}/ports/icu/" DESTINATION "${CMAKE_CURRENT_LIST_DIR}" PATTERN "portfile.cmake" EXCLUDE)
 
-# Patch upstream portfile.cmake to add emscripten args for threads mode
+# Patch upstream portfile.cmake to add emscripten args for threads mode and PIC always on
 file(READ "${VCPKG_ROOT_DIR}/ports/icu/portfile.cmake" upstream_content)
 if(VCPKG_TARGET_IS_EMSCRIPTEN)
     string(REPLACE "vcpkg_list(SET CONFIGURE_OPTIONS)" "vcpkg_list(SET CONFIGURE_OPTIONS)\nvcpkg_list(APPEND CONFIGURE_OPTIONS \"--with-pic\" \"CFLAGS=-matomics -mbulk-memory\" \"CXXFLAGS=-matomics -mbulk-memory\")" upstream_content "${upstream_content}")
--- vcpkg_ports/sqlite3/portfile.cmake
+++ vcpkg_ports/sqlite3/portfile.cmake
@@ -6,10 +6,10 @@ file(READ "${VCPKG_ROOT_DIR}/ports/sqlite3/portfile.cmake" upstream_content)
 string(REPLACE "\${CMAKE_CURRENT_LIST_DIR}" "${CMAKE_CURRENT_LIST_DIR}" upstream_content "${upstream_content}")
 file(WRITE "${CMAKE_CURRENT_LIST_DIR}/portfile_upstream_patched.cmake" "${upstream_content}")
 
-# Patch upstream CMakeLists.txt to add emscripten args for threads mode
+# Patch upstream portfile.cmake to add emscripten args for threads mode and PIC always on
 file(READ "${VCPKG_ROOT_DIR}/ports/sqlite3/CMakeLists.txt" upstream_content)
 if(VCPKG_TARGET_IS_EMSCRIPTEN)
-    string(APPEND upstream_content "\ntarget_compile_options(sqlite3 PRIVATE -matomics -mbulk-memory)")
+    string(APPEND upstream_content "\ntarget_compile_options(sqlite3 PRIVATE -fPIC -matomics -mbulk-memory)")
 endif()
 file(WRITE "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" "${upstream_content}")
 
