4663ec758363e10efc44b08e97286d22ed2b067e
Yeicor <yeicor+no-reply@github.com>
2026-02-25 21:59:53 +0100

    Update portfile.cmake

--- vcpkg_ports/icu/portfile.cmake
+++ vcpkg_ports/icu/portfile.cmake
@@ -4,7 +4,7 @@ file(COPY "${VCPKG_ROOT_DIR}/ports/icu/" DESTINATION "${CMAKE_CURRENT_LIST_DIR}"
 # Patch upstream portfile.cmake to add emscripten args for threads mode and PIC always on
 file(READ "${VCPKG_ROOT_DIR}/ports/icu/portfile.cmake" upstream_content)
 if(VCPKG_TARGET_IS_EMSCRIPTEN)
-    string(REPLACE "vcpkg_list(SET CONFIGURE_OPTIONS)" "vcpkg_list(SET CONFIGURE_OPTIONS)\nvcpkg_list(APPEND CONFIGURE_OPTIONS \"--with-pic\" \"CFLAGS=-matomics -mbulk-memory\" \"CXXFLAGS=-matomics -mbulk-memory\" \"LDFLAGS=-matomics -mbulk-memory\")" upstream_content "${upstream_content}")
+    string(REPLACE "vcpkg_list(SET CONFIGURE_OPTIONS)" "vcpkg_list(SET CONFIGURE_OPTIONS)\nvcpkg_list(APPEND CONFIGURE_OPTIONS \"--with-pic\" \"CFLAGS=-fPIC -matomics -mbulk-memory\" \"CXXFLAGS=-fPIC -matomics -mbulk-memory\" \"LDFLAGS=-fPIC -matomics -mbulk-memory\")" upstream_content "${upstream_content}")
 endif()
 file(WRITE "${CMAKE_CURRENT_LIST_DIR}/portfile_upstream_patched.cmake" "${upstream_content}")
 
