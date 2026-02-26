30bdcc687155c09f86bd3f4a776f85a289ac2c0a
Yeicor <yeicor+no-reply@github.com>
2026-02-26 15:16:35 +0100

    Remove MinGW cross build and related icu patch

--- vcpkg_ports/icu/portfile.cmake
+++ vcpkg_ports/icu/portfile.cmake
@@ -6,14 +6,9 @@ file(READ "${VCPKG_ROOT_DIR}/ports/icu/portfile.cmake" upstream_content)
 if(VCPKG_TARGET_IS_EMSCRIPTEN)
     string(REPLACE "vcpkg_list(SET CONFIGURE_OPTIONS)" "vcpkg_list(SET CONFIGURE_OPTIONS)\nvcpkg_list(APPEND CONFIGURE_OPTIONS \"--with-pic\" \"CFLAGS=-fPIC -matomics -mbulk-memory\" \"CXXFLAGS=-fPIC -matomics -mbulk-memory\" \"LDFLAGS=-fPIC -matomics -mbulk-memory\")" upstream_content "${upstream_content}")
 endif()
-if(VCPKG_TARGET_IS_MINGW)
-    string(REPLACE "if(CMAKE_HOST_WIN32 AND VCPKG_TARGET_IS_MINGW AND NOT HOST_TRIPLET MATCHES \"mingw\")" "if(FALSE AND CMAKE_HOST_WIN32 AND VCPKG_TARGET_IS_MINGW AND NOT HOST_TRIPLET MATCHES \"mingw\")" upstream_content "${upstream_content}")
-endif()
-string(REPLACE "PATCHES" "PATCHES use-slash-in-pkgdata-for-mingw-cross.patch" upstream_content "${upstream_content}")
 if(VCPKG_TARGET_IS_IOS)
     string(REPLACE "vcpkg_list(SET CONFIGURE_OPTIONS)" "vcpkg_list(SET CONFIGURE_OPTIONS)\nvcpkg_list(APPEND CONFIGURE_OPTIONS \"--host=arm64-apple-darwin\")" upstream_content "${upstream_content}")
 endif()
-
 file(WRITE "${CMAKE_CURRENT_LIST_DIR}/portfile_upstream_patched.cmake" "${upstream_content}")
 
 # Include the patched portfile.cmake
